215324cb9abbb7bc6e5a321a0c4019dbbd1af9c0
unknown <zhang3611@qq.com>
2019-06-09 16:34:49 +0800

    remote

--- cmd.go
+++ cmd.go
@@ -2,39 +2,36 @@ package app
 
 import (
 	rice "github.com/GeertJohan/go.rice"
-	"github.com/urfave/cli"
-	"github.com/zhanghup/go-framework/action"
 	"github.com/zhanghup/go-framework/ctx"
-	"os"
 )
 
-func StartCmd(afg ctx.ICfg, box *rice.Box, cmds []cli.Command) {
-	ctx.InitCfg(afg, box)
-	appconfig := ctx.GetCfg()
-	if appconfig == nil {
-		panic("配置文件未读取")
-	}
-	//2. 实例化midway代理
-	cmd := cli.NewApp()
-	cmd.Name = appconfig.System.Name
-	cmd.Description = appconfig.System.Brief
-	cmd.Version = appconfig.System.Version
-	cmd.Action = func(c *cli.Context) error {
-		return action.Run()
-	}
-
-	cmd.Commands = []cli.Command{
-
-	}
-
-	if len(cmds) > 0 {
-		cmd.Commands = append(cmd.Commands, cmds...)
-	}
-
-	if err := cmd.Run(os.Args); err != nil {
-		panic(err.Error())
-	}
-}
+//func StartCmd(afg ctx.ICfg, box *rice.Box, cmds []cli.Command) {
+//	ctx.InitCfg(afg, box)
+//	appconfig := ctx.GetCfg()
+//	if appconfig == nil {
+//		panic("配置文件未读取")
+//	}
+//	//2. 实例化midway代理
+//	cmd := cli.NewApp()
+//	cmd.Name = appconfig.System.Name
+//	cmd.Description = appconfig.System.Brief
+//	cmd.Version = appconfig.System.Version
+//	cmd.Action = func(c *cli.Context) error {
+//		return action.Run()
+//	}
+//
+//	cmd.Commands = []cli.Command{
+//
+//	}
+//
+//	if len(cmds) > 0 {
+//		cmd.Commands = append(cmd.Commands, cmds...)
+//	}
+//
+//	if err := cmd.Run(os.Args); err != nil {
+//		panic(err.Error())
+//	}
+//}
 
 func StartCommon(afg ctx.ICfg, box *rice.Box) {
 	ctx.InitCfg(afg, box)
